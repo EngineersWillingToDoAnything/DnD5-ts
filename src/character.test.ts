0f8b1c2eb92f8df80414a4b2c1815b6a36361d64
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-12-22 21:50:25 +0000

    feat: chracter health's system

--- src/character.test.ts
+++ src/character.test.ts
@@ -5,6 +5,11 @@ beforeAll(() => {
   testCharacter = new Character('John Doe');
 })
 
+beforeEach(() => {
+  testCharacter.resetHealth();
+});
+
+
 describe ('Character properties tests', () => {
   it('Should have a name', () => {
     expect(testCharacter.name).toBe('John Doe');
@@ -12,5 +17,39 @@ describe ('Character properties tests', () => {
 
   it ('Should start with level 1', () => {
     expect(testCharacter.level).toBe(1);
+  });
+
+  it ('Should start with 3 health points', () => {
+    expect(testCharacter.healthPoints).toBe(3);
+  });
+});
+
+
+describe ('Character health system', () => {
+  it ('Should be able to take damage', () => {
+    testCharacter.takeDamage(1);
+    expect(testCharacter.healthPoints).toBe(2);
+  });
+
+  it ('Should not be able to have negative health points', () => {
+    testCharacter.takeDamage(10);
+    expect(testCharacter.healthPoints).toBe(0);
+  });
+
+  it ('Should be able to reset health points', () => {
+    testCharacter.takeDamage(1);
+    testCharacter.resetHealth();
+    expect(testCharacter.healthPoints).toBe(3);
+  });
+
+  it ('Should be able to heal', () => {
+    testCharacter.takeDamage(2);
+    testCharacter.heal(1);
+    expect(testCharacter.healthPoints).toBe(2);
+  });
+
+  it ('Should not be able to heal over 3 health points', () => {
+    testCharacter.heal(10);
+    expect(testCharacter.healthPoints).toBe(3);
   });
 });
--- src/character.ts
+++ src/character.ts
@@ -1,11 +1,12 @@
 /**
  * @classdesc Represent a D&D character
  *
- * @exports
+ * @export
  * @default
  */
 export default class Character {
   public level: number = 1;
+  public healthPoints: number = 3;
 
   /**
    * Create a new DnD character
@@ -13,5 +14,32 @@ export default class Character {
    */
   constructor(public readonly name: string) {
     this.name = name;
+  }
+
+  /**
+   * @brief Recieve a certain amount of damage
+   *
+   * @param damage The amount of damage to take
+   */
+  public takeDamage(damage: number) {
+    this.healthPoints -= damage;
+    if (this.healthPoints < 0) this.healthPoints = 0;
+  }
+
+  /**
+   * @brief Reset the health points to the maximum
+   */
+  public resetHealth() {
+    this.healthPoints = 3;
+  }
+
+  /**
+   * @brief Heal the character by a certain amount
+   *
+   * @param healAmount The amount of health to heal
+   */
+  public heal(healAmount: number) {
+    this.healthPoints += healAmount;
+    if (this.healthPoints > 3) this.healthPoints = 3;
   }
 }
